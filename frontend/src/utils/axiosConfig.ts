14390e52346171b2fe8280f6b651042f543ecb85
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2025-11-27 13:46:31 +0530

    Prepared frontend for production

--- frontend/src/utils/axiosConfig.ts
+++ frontend/src/utils/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { authAPI } from "../services/common/authApi";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:5000/api",
   timeout: 30000,
 });
 
