fcef862663245fd43558232516a3fd3f900ec19e
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2025-11-27 10:21:20 +0530

    test response type

--- backend/src/controllers/NotificationController.ts
+++ backend/src/controllers/NotificationController.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express-serve-static-core';
+import type { Response } from 'express';
 import { INotificationService } from '../interfaces/services/INotificationService';
 import { ResponseHelper } from '../utils/responseHelper';
 import { ILogger } from '@/interfaces/utils/ILogger';
