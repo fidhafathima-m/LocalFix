2d5bea76d97e394b788355ce7761482eda727567
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2026-02-07 11:06:56 +0530

    Remove backend dist from repository

--- backend/dist/routes/technician/technicianChatRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../../middleware/authMiddleware");
-const container_1 = require("../../config/container");
-const router = (0, express_1.Router)();
-router.post('/message', authMiddleware_1.protect, container_1.technicianChatController.sendMessage);
-exports.default = router;
